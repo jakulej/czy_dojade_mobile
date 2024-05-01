fb666f4fa00275e519bc3ea0757e7826660a5f07
Dmitry_Golt <dimagolt@gmail.com>
2024-05-01 12:02:13 +0200

    add transport to maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:czy_dojade/widgets/lines_bottom_sheet.dart';
 import 'package:flutter/material.dart' hide FilterChip;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../models/transport.dart';
 import '../widgets/filter_chip.dart';
 
 class MyHomeScreen extends StatefulWidget {
@@ -16,6 +18,7 @@ class MyHomeScreen extends StatefulWidget {
 }
 
 class _MyHomeScreenState extends State<MyHomeScreen> {
+  List<Transport> transports = [];
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
@@ -39,6 +42,20 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
   };
 
   @override
+  void initState() {
+    loadTransports();
+    super.initState();
+  }
+
+  loadTransports() async {
+    String data = await DefaultAssetBundle.of(context)
+        .loadString("assets/mocks/wroclaw-live2.json");
+    Map<String, dynamic> json = jsonDecode(data)['data'];
+    transports = json.values.map((e) => Transport.fromJson(e)).toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
@@ -52,14 +69,19 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
             initialCameraPosition: _breslau,
             buildingsEnabled: false,
             zoomControlsEnabled: false,
+            markers: transports.map((e) => e.mapMarker).toSet(),
           ),
           SafeArea(child: _searchAndFilterBar()),
           Align(
             alignment: Alignment.centerLeft,
             child: Material(
-              borderRadius: const BorderRadius.only(topRight: Radius.circular(12), bottomRight: Radius.circular(12)),
+              borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(12),
+                  bottomRight: Radius.circular(12)),
               child: InkWell(
-                borderRadius: const BorderRadius.only(topRight: Radius.circular(12), bottomRight: Radius.circular(12)),
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(12),
+                    bottomRight: Radius.circular(12)),
                 onTap: () {
                   _scaffoldKey.currentState?.openDrawer();
                 },
@@ -119,28 +141,47 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
     return Container(
       color: Colors.white,
       width: 250,
-      child:  SafeArea(
+      child: SafeArea(
         child: Column(
           children: [
             const ListTile(
-              leading: Icon(Icons.account_circle, size: 56,),
-              title: Text('John Doe', style: TextStyle(fontSize: 24),),
+              leading: Icon(
+                Icons.account_circle,
+                size: 56,
+              ),
+              title: Text(
+                'John Doe',
+                style: TextStyle(fontSize: 24),
+              ),
               iconColor: Colors.black,
               textColor: Colors.black,
             ),
-            const Divider(thickness: 3,),
+            const Divider(
+              thickness: 3,
+            ),
             ListTile(
-              leading: const Icon(Icons.manage_accounts_rounded, size: 52,),
-              title: const Text('Profile', style: TextStyle(fontSize: 18),),
-              onTap: () {
-              },
+              leading: const Icon(
+                Icons.manage_accounts_rounded,
+                size: 52,
+              ),
+              title: const Text(
+                'Profile',
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {},
               iconColor: Colors.black,
               textColor: Colors.black,
             ),
             const Divider(),
             ListTile(
-              leading: const Icon(Icons.settings, size: 52,),
-              title: const Text('Settings', style: TextStyle(fontSize: 18),),
+              leading: const Icon(
+                Icons.settings,
+                size: 52,
+              ),
+              title: const Text(
+                'Settings',
+                style: TextStyle(fontSize: 18),
+              ),
               onTap: () {},
               iconColor: Colors.black,
               textColor: Colors.black,
@@ -149,8 +190,14 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
             const Expanded(child: SizedBox()),
             const Divider(),
             ListTile(
-              leading: const Icon(Icons.power_settings_new, size: 52,),
-              title: const Text('Sign out', style: TextStyle(fontSize: 18),),
+              leading: const Icon(
+                Icons.power_settings_new,
+                size: 52,
+              ),
+              title: const Text(
+                'Sign out',
+                style: TextStyle(fontSize: 18),
+              ),
               onTap: () {},
               iconColor: Colors.red,
               textColor: Colors.red,
